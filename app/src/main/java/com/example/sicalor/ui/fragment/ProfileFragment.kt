bc779c75ef0564fd39324a5e5570fb097fc27726
parhannn <farhannn58@gmail.com>
2025-03-19 14:56:56 +0700

    add user data reference on profile fragment

--- app/src/main/java/com/example/sicalor/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/sicalor/ui/fragment/ProfileFragment.kt
@@ -5,13 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.sicalor.databinding.FragmentProfileBinding
+import com.example.sicalor.ui.data.UserData
 import com.example.sicalor.ui.user.FormActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userId: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,9 +38,38 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+        userId = auth.currentUser!!.uid
+        database = Firebase.database.reference.child("UserData")
+            .child(userId)
+
+        getUserData()
+
         binding.updateData.setOnClickListener {
             val intent = Intent(requireContext(), FormActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getUserData() {
+        database.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (userSnapshot in snapshot.children) {
+                    val userData = userSnapshot.getValue(UserData::class.java)
+
+                    if (userData != null) {
+                        binding.tvName.text = userData.name
+                        binding.tvGender.text = userData.gender
+                        binding.tvAge.text = userData.age
+                        binding.tvWeight.text = userData.weight
+                        binding.tvHeight.text = userData.height
+                        binding.tvActivity.text = userData.activity
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
