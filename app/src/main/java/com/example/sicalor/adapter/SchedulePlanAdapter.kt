bf27a46dcb5e48b4782bea87a3276fd757ba0026
parhannn <farhannn58@gmail.com>
2025-03-25 18:36:47 +0700

    bug fixes

---
+++ app/src/main/java/com/example/sicalor/adapter/SchedulePlanAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.sicalor.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.sicalor.R
+import com.example.sicalor.databinding.ItemMealPlanBinding
+import com.example.sicalor.ui.data.MealData
+import com.example.sicalor.ui.data.MealPlanData
+
+class SchedulePlanAdapter(
+    private val context: Context,
+    private var schedulePlanList: List<MealPlanData>,
+    private var mealList: List<MealData>
+) :RecyclerView.Adapter<SchedulePlanAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMealPlanBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val schedulePlan = schedulePlanList[position]
+        val meal = mealList.find { it.name == schedulePlan.mealData.name }
+        if (meal != null) {
+            holder.bind(schedulePlan, meal)
+        }
+    }
+
+    override fun getItemCount(): Int = schedulePlanList.size
+
+    fun updateData(newSchedulePlanList: List<MealPlanData>, newMealList: List<MealData>){
+        schedulePlanList = newSchedulePlanList
+        mealList = newMealList
+        notifyDataSetChanged()
+        Toast.makeText(context, "Data Updated!", Toast.LENGTH_SHORT).show()
+    }
+
+    inner class ViewHolder(private val binding: ItemMealPlanBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(schedulePlan: MealPlanData, meal: MealData) {
+            binding.foodName.text = meal.name
+            binding.foodCalories.text = "${meal.calories} kcal"
+            binding.foodPortion.text = "${meal.portion} g"
+            binding.foodType.text = schedulePlan.type
+            Glide.with(binding.foodImage.context)
+                .load(meal.img)
+                .placeholder(R.drawable.ic_food_1)
+                .into(binding.foodImage)
+        }
+    }
+}
--- app/src/main/java/com/example/sicalor/ui/fragment/ScheduleFragment.kt
+++ app/src/main/java/com/example/sicalor/ui/fragment/ScheduleFragment.kt
@@ -1,24 +1,45 @@
 package com.example.sicalor.ui.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sicalor.adapter.SchedulePlanAdapter
 import com.example.sicalor.databinding.FragmentScheduleBinding
 import com.example.sicalor.ui.data.MealData
 import com.example.sicalor.ui.data.MealPlanData
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
+@SuppressLint("SimpleDateFormat")
 class ScheduleFragment : Fragment() {
     private lateinit var database: DatabaseReference
+    private lateinit var adapter: SchedulePlanAdapter
+    private lateinit var recyclerView: RecyclerView
     private lateinit var auth: FirebaseAuth
     private lateinit var userId: String
     private lateinit var mealPlanData: MealPlanData
     private lateinit var mealData: MealData
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
+    private var dateMealToday: String = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date()).toString()
+    private var selectedDate: String = ""
+    private var allPlanList: List<MealPlanData> = emptyList()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,16 +53,102 @@ class ScheduleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         authUser()
+        setupUI()
+    }
 
+    private fun setupUI() {
         binding.addButton.setOnClickListener { showSheetDialog() }
+        binding.selectedDate.text = dateMealToday
+        setupDatePicker()
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = SchedulePlanAdapter(requireContext(), mutableListOf(), mutableListOf())
+        recyclerView = binding.mealPlanRecyclerview
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun loadMealPlan() {
+        database = Firebase.database.reference.child("MealPlanData")
+
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val mealPlanDataList = mutableListOf<MealPlanData>()
+                    val mealDataList = mutableListOf<MealData>()
+
+                    for (userSnapshot in snapshot.children){
+                        for (mealPlanDataSnapshot in userSnapshot.children) {
+                            val mealPlanData = mealPlanDataSnapshot.getValue(MealPlanData::class.java)
+                            if (mealPlanData != null) {
+                                mealPlanDataList.add(mealPlanData)
+                                mealDataList.add(mealPlanData.mealData)
+                            }
+                        }
+                    }
+                    allPlanList = mealPlanDataList
+                    adapter.updateData(mealPlanDataList, mealDataList)
+                } else {
+                    Log.d("DEBUG", "No data available")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FirebaseError", "Error: ${error.message}")
+            }
+
+        })
+
+//        database.child(userId).child("mealId").child(dateMealToday).get().addOnSuccessListener { snapshot ->
+//            if (snapshot.exists()) {
+//                val mealPlanDataList = mutableListOf<MealPlanData>()
+//                val mealDataList = mutableListOf<MealData>()
+//
+//                for (mealPlanDataSnapshot in snapshot.children) {
+//                    val mealPlanData = mealPlanDataSnapshot.getValue(MealPlanData::class.java)
+//                    if (mealPlanData != null) {
+//                        mealPlanDataList.add(mealPlanData)
+//                        mealDataList.add(mealPlanData.mealData)
+//                    }
+//                }
+//                allPlanList = mealPlanDataList
+//                adapter.updateData(mealPlanDataList, mealDataList)
+//            } else {
+//                Log.d("DEBUG", "No data available")
+//            }
+//        }.addOnFailureListener {
+//            Log.e("FirebaseError", "Error: ${it.message}")
+//        }
     }
 
     private fun showSheetDialog() {
         val sheetDialog = AddMealFragment()
 
         sheetDialog.show(parentFragmentManager, "TAG")
+    }
+
+    private fun setupDatePicker() {
+        binding.selectDate.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Meal Plan Date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+            datePicker.show(parentFragmentManager, "DATE_PICKER")
+
+            datePicker.addOnPositiveButtonClickListener { selection ->
+                selectedDate =
+                    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date(selection))
+                binding.selectedDate.text = selectedDate
+                Log.d("DEBUG", "Selected Date: $selectedDate")
+                loadMealPlan()
+            }
+        }
+
+        loadMealPlan()
     }
 
     private fun authUser() {
